0437452cfd06ec1437e66d83734cc907908b0f33
Mabilikoe Thoahlane <thoahlane10111@gmail.com>
2023-06-15 12:12:01 -0700

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.hs>
+/**
+ * Main - Program which prints a line using puts fucnction
+ * Return (0)
+ * /
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
