84c856bf008a7c0ec0c2fe2b366d5a1da38a1a3f
Mabilikoe Thoahlane <thoahlane10111@gmail.com>
2023-06-16 16:50:26 -0700

    prints

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,9 +10,9 @@ int main(void)
 char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
-	for (i = 0; i < 52; i++;)
+	for (i = 0; i < 52; i++);
 	{
-		putchar(alp[i];
+		putchar(alp[i]);
 	}
 	putchar('\n');
 	return (0);
