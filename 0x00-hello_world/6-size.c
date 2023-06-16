5a7558eadeef67d7f38bd0aad71280b151f7d492
Mabilikoe Thoahlane <thoahlane10111@gmail.com>
2023-06-15 17:20:17 -0700

    betty

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,11 +5,11 @@
  */
 int main(void)
 {
-	printf("size of a char: %zd byte(s)\n", sizeof(char));
-	printf("size of a char: %zd byte(s)\n", sizeof(int));
-	printf("size of a long int: %zd byte(s)\n", sizeof(long int));
-	printf("size of a long long int: %zd byte(s)\n", sizeof(long long int));
-	printf("size of a float: %zd byte(s)\n", sizeof(float));
+	printf("size of a char: %d byte(s)\n", sizeof(char));
+	printf("size of a char: %d byte(s)\n", sizeof(int));
+	printf("size of a long int: %#d byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
 
