ff706e83b4ae6dcee9d22a55dbbb214c210c70aa
Mabilikoe Thoahlane <thoahlane10111@gmail.com>
2023-06-16 19:37:09 -0700

    48

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,12 +8,12 @@ int main(void)
 {
 	int n;
 
-	for (n = 8; n < 58; n++)
+	for (n = 48; n < 58; n++)
 	{
 		putchar(n);
 		if (n != 57)
 		{
-			putchar(',');
+			putchar('.');
 			putchar(' ');
 		}
 	}
