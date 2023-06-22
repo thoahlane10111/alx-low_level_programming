05dead4c2fe01bc0e77180ec5efc022e90900859
Mabilikoe Thoahlane <thoahlane10111@gmail.com>
2023-06-22 16:41:51 -0700

    debugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,6 +9,7 @@
 int largest_number(int a, int b, int c)
 	{
 	int largest;
+
 	if (a > b && a > c)
 	{
 		largest = a;
@@ -24,7 +25,7 @@ int largest_number(int a, int b, int c)
 	else
 	{
 		largest = c;
-	}	
+	}
 	return (largest);
 }
 
