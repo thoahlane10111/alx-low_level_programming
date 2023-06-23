4b47f87b68a9637f8e314568efaa28dcb8728b3d
Mabilikoe Thoahlane <thoahlane10111@gmail.com>
2023-06-22 17:02:16 -0700

    debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,10 @@
 #include "main.h"
+
 /**
  * main - Test function for positive or negative
  * Return: 0
  */
+
 int main(void)
 {
 	int i;
@@ -10,5 +12,6 @@ int main(void)
 	i = 0;
 
 	positive_or_negative(i);
+
 	return (0);
 }
